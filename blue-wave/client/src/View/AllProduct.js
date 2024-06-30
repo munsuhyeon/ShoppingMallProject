eda1fc1818511e91ab9829420a41285a89ae3eed
munsuhyeon <hrno3826@naver.com>
2024-07-01 02:31:24 +0900

    Fix: 상품불러오기 주소 오류 수정

--- blue-wave/client/src/View/AllProduct.js
+++ blue-wave/client/src/View/AllProduct.js
@@ -26,8 +26,8 @@ const CategoryProducts = () => {
     async function fetchData() {
       try {
         const endpoint = subCategoryId
-          ? `${process.env.REACT_APP_HOST}/${categoryId}/${subCategoryId}`
-          : `${process.env.REACT_APP_HOST}/${categoryId}`;
+          ? `${process.env.REACT_APP_HOST}/product/${categoryId}/${subCategoryId}`
+          : `${process.env.REACT_APP_HOST}/product/${categoryId}`;
 
         const response = await axios.get(endpoint);
         setProducts(response.data);
